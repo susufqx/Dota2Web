38cc8070aa3d25c12e4acf8e2e6d026efa2c51cd
susufqx <jiangsulirui@gmail.com>
2017-09-14 11:14:20 +0200

    Create the example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'dota2-angular',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div>
+      <label>id: </label>
+      {{hero.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)] = 'hero.name' placeholder = 'name'>
+    </div>`,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'Welcome to dota2 space!';
+  title = 'Tour of heroes';
+  hero : Hero = {
+    id : 1,
+    name : 'Windstorm'
+  };
+}
+
+export class Hero {
+  id : number;
+  name : String;
 }
